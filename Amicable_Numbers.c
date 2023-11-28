b2d2134ab19c58ce4be371dc059f2e5614b6dcef
divya123s <147487538+divya123s@users.noreply.github.com>
2023-11-28 11:36:44 +0530

    #include<stdio.h> int main() {     int a,b,i,s=0,t=0;     scanf("%d%d",&a,&b);     for(i=1;i<a;i++)     {         if(a%i==0)         {             s=s+i;         }     }     for(i=1;i<b;i++)     {         if(b%i==0)         {             t=t+i;         }     }     if(s==b || t==a)     {         printf("Amicable");     }     else     {         printf("Not Amicable");     } }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,s=0,t=0;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            s=s+i;
+        }
+    }
+    for(i=1;i<b;i++)
+    {
+        if(b%i==0)
+        {
+            t=t+i;
+        }
+    }
+    if(s==b || t==a)
+    {
+        printf("Amicable");
+    }
+    else
+    {
+        printf("Not Amicable");
+    }
+}
